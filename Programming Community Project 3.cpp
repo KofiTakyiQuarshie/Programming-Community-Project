23ce495d5233a1a2b89ea5734d653fd689fa7e18
KofiTakyiQuarshie <102687691+KofiTakyiQuarshie@users.noreply.github.com>
2022-05-31 13:27:30 +0000

    Add files via upload

---
+++ Programming Community Project 3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+
+{    int Age;
+     int NewAge;
+
+     cout << "Enter your Age: " << endl;
+     cin >> Age;
+
+     NewAge = Age + 5;
+     cout << "The supposedly New Age is " << NewAge << endl;
+
+
+
+    return 0;
+}
